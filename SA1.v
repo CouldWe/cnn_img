2448073c48dc8be38663354a407336b744fe64ce
kang_peng12 <1492457567@qq.com>
2026-04-05 15:34:26 +0800

    第一次提交

---
+++ SA1.v
@@ -0,0 +1,25 @@
+module SA1(
+    input wire clk,
+    input wire rst_n,
+    input wire sa1_control, //SA1控制信号，右移时为1
+    input wire signed [56-1:0] data_in,   //7*8bit输入数据
+    input wire signed [32*616-1:0] weight,  //32*11*7*8bit权重拼接
+    output wire signed [32*32-1:0] data_out //32*32bit输出拼接
+);
+
+    genvar i;
+    //实例化32个SA1_channel
+    generate
+        for (i = 0; i < 32; i = i + 1) begin : sa1_channels
+            SA1_channel u_SA1_channel (
+                .clk(clk),
+                .rst_n(rst_n),
+                .sa1_control(sa1_control), //SA1控制信号，右移时为1
+                .data_in(data_in),   //输入数据广播到所有通道   
+                .weight(weight[i*616 +615: i*616]),   
+                .data_out(data_out[i*32 +31: i*32])    
+            );
+        end
+    endgenerate
+
+endmodule
